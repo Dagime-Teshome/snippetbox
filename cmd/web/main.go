b32ca28edd6474f085d0943cb0591b5d3adf4d2b
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-04 23:03:51 +0300

    modify main with new updates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,28 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	snippets "github.com/Dagime-Teshome/snippetbox/pkg/models/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippet  *snippets.SnippetModel
 }
 
 func main() {
+
 	infoLog := log.New(os.Stdout, "Info:\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "Error:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	dsn := flag.String("dsn", "root:root@/snippetbox?parseTime=true", "connection string for database")
 	addr := flag.String("addr", ":3001", "port for server")
 	flag.Parse()
-	fmt.Println("hello")
-
+	db, err := opeDb(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 	app := application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippet:  &snippets.SnippetModel{Db: db},
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
@@ -34,4 +44,15 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		errorLog.Fatal("server:%w", err)
 	}
+}
+
+func opeDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
