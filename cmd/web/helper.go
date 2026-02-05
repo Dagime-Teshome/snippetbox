080a8546528be202324bcd266ea34d058c6b3cfe
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-05 15:15:33 +0300

    add template helper function to return templates

--- cmd/web/helper.go
+++ cmd/web/helper.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"runtime/debug"
 )
@@ -21,5 +22,37 @@ func (app *application) ClientError(w http.ResponseWriter, status int) {
 }
 
 func (app *application) NotFound(w http.ResponseWriter) {
-	app.ClientError(w, http.StatusNotFound)
+	template, err := returnTemplate("")
+	if err != nil {
+		app.ServerError(w, err)
+		return
+	}
+	if err := template.Execute(w, nil); err != nil {
+		app.ServerError(w, err)
+		return
+	}
+	// app.ClientError(w, http.StatusNotFound)
+}
+
+func returnTemplate(page string) (*template.Template, error) {
+	templatePath := ""
+	switch page {
+	case "home":
+		templatePath = "./ui/html/home.page.tmpl"
+	case "snippet":
+		templatePath = "./ui/html/show.page.tmpl"
+	default:
+		templatePath = "./ui/html/404.page.tmpl"
+	}
+
+	files := []string{
+		templatePath,
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	template, err := template.ParseFiles(files...)
+	if err != nil {
+		return nil, err
+	}
+	return template, nil
 }
