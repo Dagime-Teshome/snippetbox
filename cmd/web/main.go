bd3156efa6c4bd9daccaef3929721cbd2a56efaf
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-06 14:56:46 +0300

    inject template cache in to main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,9 +13,10 @@ import (
 )
 
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippet  *snippets.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippet       *snippets.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -29,10 +31,16 @@ func main() {
 		errorLog.Fatal(err)
 	}
 	defer db.Close()
+	templateCache, err := newTemplateCache("./ui/html/")
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 	app := application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippet:  &snippets.SnippetModel{Db: db},
+
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippet:       &snippets.SnippetModel{Db: db},
+		templateCache: templateCache,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
