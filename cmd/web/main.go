ad5f8130b06cc37eac827bfc4e5c424ac129005d
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-16 21:50:55 +0300

    initialize sessions in main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	snippets "github.com/Dagime-Teshome/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golangcollege/sessions"
 )
 
 type application struct {
@@ -17,6 +19,7 @@ type application struct {
 	infoLog       *log.Logger
 	snippet       *snippets.SnippetModel
 	templateCache map[string]*template.Template
+	Session       *sessions.Session
 }
 
 func main() {
@@ -25,6 +28,7 @@ func main() {
 	errorLog := log.New(os.Stderr, "Error:\t", log.Ldate|log.Ltime|log.Lshortfile)
 	dsn := flag.String("dsn", "root:root@/snippetbox?parseTime=true", "connection string for database")
 	addr := flag.String("addr", ":3001", "port for server")
+	secret := flag.String("secret", "xK7vQp3Lz9R4mN8tY2cHf6Bq1Ws5Dj0U", "secret to encrypt session")
 	flag.Parse()
 	db, err := opeDb(*dsn)
 	if err != nil {
@@ -35,12 +39,15 @@ func main() {
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = time.Minute * 30
 	app := application{
 
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippet:       &snippets.SnippetModel{Db: db},
 		templateCache: templateCache,
+		Session:       session,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
