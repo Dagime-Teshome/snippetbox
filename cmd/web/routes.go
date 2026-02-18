03a1d17579b6525f05d64771034ddccccbe1ad1a
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-18 17:10:53 +0300

    add new sigin and logout routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,11 @@ func (app *application) routes() http.Handler {
 	mux.Get("/snippet/create", app.Session.Enable(http.HandlerFunc(app.createSnippetForm)))
 	mux.Post("/snippet/create", app.Session.Enable(http.HandlerFunc(app.createSnippet)))
 	mux.Get("/snippets/list", app.Session.Enable(http.HandlerFunc(app.listSnippets)))
+	mux.Get("/user/login", app.Session.Enable(http.HandlerFunc(app.loginUserForm)))
+	mux.Post("/user/login", app.Session.Enable(http.HandlerFunc(app.loginUser)))
+	mux.Get("/user/signup", app.Session.Enable(http.HandlerFunc(app.signupUserForm)))
+	mux.Post("/user/signup", app.Session.Enable(http.HandlerFunc(app.signupUser)))
+	mux.Post("/user/logout", app.Session.Enable(http.HandlerFunc(app.logoutUser)))
 	mux.Get("/snippet/:id", app.Session.Enable(http.HandlerFunc(app.showSnippet)))
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
