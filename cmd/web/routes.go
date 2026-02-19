9d7f406ac67efb6436f37568bf856803a77514f1
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-19 21:45:42 +0300

    use authorization check middleware on routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,15 +10,20 @@ func (app *application) routes() http.Handler {
 
 	mux := pat.New()
 	mux.Get("/", app.Session.Enable(http.HandlerFunc(app.handleHome)))
-	mux.Get("/snippet/create", app.Session.Enable(http.HandlerFunc(app.createSnippetForm)))
-	mux.Post("/snippet/create", app.Session.Enable(http.HandlerFunc(app.createSnippet)))
 	mux.Get("/snippets/list", app.Session.Enable(http.HandlerFunc(app.listSnippets)))
+	// create snippets
+	mux.Get("/snippet/create", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.createSnippetForm))))
+	mux.Post("/snippet/create", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.createSnippet))))
+	// login
 	mux.Get("/user/login", app.Session.Enable(http.HandlerFunc(app.loginUserForm)))
 	mux.Post("/user/login", app.Session.Enable(http.HandlerFunc(app.loginUser)))
+	// sign up
 	mux.Get("/user/signup", app.Session.Enable(http.HandlerFunc(app.signupUserForm)))
 	mux.Post("/user/signup", app.Session.Enable(http.HandlerFunc(app.signupUser)))
-	mux.Post("/user/logout", app.Session.Enable(http.HandlerFunc(app.logoutUser)))
-	mux.Get("/snippet/:id", app.Session.Enable(http.HandlerFunc(app.showSnippet)))
+	// log out
+	mux.Post("/user/logout", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.logoutUser))))
+	// show snippet
+	mux.Get("/snippet/:id", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.showSnippet))))
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
