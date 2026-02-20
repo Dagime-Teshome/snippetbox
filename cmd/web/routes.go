e9720070c1b9371086671bbe8053be6139c07216
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-20 11:00:13 +0300

    use nosurf middleware on dynamic routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,18 +12,18 @@ func (app *application) routes() http.Handler {
 	mux.Get("/", app.Session.Enable(http.HandlerFunc(app.handleHome)))
 	mux.Get("/snippets/list", app.Session.Enable(http.HandlerFunc(app.listSnippets)))
 	// create snippets
-	mux.Get("/snippet/create", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.createSnippetForm))))
-	mux.Post("/snippet/create", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.createSnippet))))
+	mux.Get("/snippet/create", app.Session.Enable(app.isLoggedIn(noSurf(http.HandlerFunc(app.createSnippetForm)))))
+	mux.Post("/snippet/create", app.Session.Enable(app.isLoggedIn(noSurf(http.HandlerFunc(app.createSnippet)))))
 	// login
-	mux.Get("/user/login", app.Session.Enable(http.HandlerFunc(app.loginUserForm)))
-	mux.Post("/user/login", app.Session.Enable(http.HandlerFunc(app.loginUser)))
+	mux.Get("/user/login", app.Session.Enable(noSurf(http.HandlerFunc(app.loginUserForm))))
+	mux.Post("/user/login", app.Session.Enable(noSurf(http.HandlerFunc(app.loginUser))))
 	// sign up
-	mux.Get("/user/signup", app.Session.Enable(http.HandlerFunc(app.signupUserForm)))
-	mux.Post("/user/signup", app.Session.Enable(http.HandlerFunc(app.signupUser)))
+	mux.Get("/user/signup", app.Session.Enable(noSurf(http.HandlerFunc(app.signupUserForm))))
+	mux.Post("/user/signup", app.Session.Enable(noSurf(http.HandlerFunc(app.signupUser))))
 	// log out
-	mux.Post("/user/logout", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.logoutUser))))
+	mux.Post("/user/logout", app.Session.Enable(app.isLoggedIn(noSurf(http.HandlerFunc(app.logoutUser)))))
 	// show snippet
-	mux.Get("/snippet/:id", app.Session.Enable(app.isLoggedIn(http.HandlerFunc(app.showSnippet))))
+	mux.Get("/snippet/:id", app.Session.Enable(app.isLoggedIn(noSurf(http.HandlerFunc(app.showSnippet)))))
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
