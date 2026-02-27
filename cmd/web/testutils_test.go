b12f36ff9995356e8f45cbc3be1a2b69f509825b
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-27 13:18:44 +0300

    initialize mock dependencies for test

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -7,13 +7,32 @@ import (
 	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/Dagime-Teshome/snippetbox/pkg/models/mock"
+	"github.com/golangcollege/sessions"
 )
 
 func newTestApplication(t *testing.T) *application {
 
+	templateCache, err := newTemplateCache("./../../ui/html/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	secret := "xK7vQp3Lz9R4mN8tY2cHf6Bq1Ws5Dj0U"
+	// Create a session manager instance, with the same settings as production.
+	session := sessions.New([]byte(secret))
+	session.Lifetime = time.Minute * 30
+	session.SameSite = http.SameSiteStrictMode
+	session.Secure = true
+
 	return &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
+		errorLog:      log.New(io.Discard, "", 0),
+		infoLog:       log.New(io.Discard, "", 0),
+		Session:       session,
+		snippet:       &mock.SnippetModel{},
+		user:          &mock.UserModel{},
+		templateCache: templateCache,
 	}
 }
 
