3126ae59e44171cbd97b3a43a2e4d1c3f0ae6683
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-27 13:13:39 +0300

    add show-snippet test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -17,6 +18,49 @@ func TestPing(t *testing.T) {
 	}
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
+	}
+
+}
+
+func TestShowSnippet(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		urlPath    string
+		wantStatus int
+		wantBody   []byte
+	}{
+		{"Valid Id", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Invalid Id", "/snippet/2", http.StatusNotFound, nil},
+		{"String Id", "/snippet/rand", http.StatusNotFound, nil},
+		{"Empty Id", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing Id", "/snippet/1/", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+	}
+	testRunner(t, tests)
+}
+
+func testRunner(t *testing.T, testList []struct {
+	name       string
+	urlPath    string
+	wantStatus int
+	wantBody   []byte
+}) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	for _, test := range testList {
+		t.Run(test.name, func(t *testing.T) {
+			status, _, body := ts.get(t, test.urlPath)
+			if status != test.wantStatus {
+				t.Errorf("wanted %d got %d", test.wantStatus, status)
+			}
+			if !bytes.Contains(body, test.wantBody) {
+				t.Errorf("wanted %s got %s", string(test.wantBody), string(body))
+			}
+		})
 	}
 
 }
