9a8f5b57d8693742fef7f56783c69073a145a79f
Dagime Teshome <dagimeteshome1@gmail.com>
2026-02-28 17:26:10 +0300

    re write form error texts

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -29,7 +29,7 @@ func (f *Form) Required(fields ...string) {
 	for _, field := range fields {
 		value := f.Get(field)
 		if strings.TrimSpace(value) == "" {
-			f.Errors.Add(field, fmt.Sprintf("%s field can not be empty", field))
+			f.Errors.Add(field, fmt.Sprintf("%s This field cannot be blank", field))
 		}
 	}
 }
@@ -44,7 +44,7 @@ func (f *Form) MaxLength(field string, limit int) {
 func (f *Form) MinLength(field string, limit int) {
 	value := f.Get(field)
 	if utf8.RuneCountInString(value) < limit {
-		f.Errors.Add(field, fmt.Sprintf("%s password must have a min length of %v", field, limit))
+		f.Errors.Add(field, fmt.Sprintf("%s Password is too short :password must have a min length of %v", field, limit))
 	}
 }
 
@@ -54,7 +54,7 @@ func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
 		return
 	}
 	if !pattern.MatchString(value) {
-		f.Errors.Add(field, "This field is invalid")
+		f.Errors.Add(field, "Enter a valid email address")
 	}
 }
 func (f *Form) PermittedValues(field string, opts ...string) {
