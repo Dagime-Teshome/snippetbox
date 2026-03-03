4723da9cd1abc48d07105ddd379a1e4a5248f653
Dagime Teshome <dagimeteshome1@gmail.com>
2026-03-03 18:35:03 +0300

    add comments to improve readability

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -41,24 +41,33 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "Info:\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "Error:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// data that a user can pass when starting the web server (data from the command line)
 	dsn := flag.String("dsn", "root:root@/snippetbox?parseTime=true", "connection string for database")
 	addr := flag.String("addr", ":3001", "port for server")
 	secret := flag.String("secret", "xK7vQp3Lz9R4mN8tY2cHf6Bq1Ws5Dj0U", "secret to encrypt session")
 	flag.Parse()
+
+	// Open Database connection
 	db, err := opeDb(*dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 	defer db.Close()
+
+	// initialize template cache
 	templateCache, err := newTemplateCache("./ui/html/")
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+
+	// setup and create session
 	session := sessions.New([]byte(*secret))
 	session.Lifetime = time.Minute * 30
 	session.SameSite = http.SameSiteStrictMode
 	session.Secure = true
 
+	// injected dependency of the app that handlers and middleware use
 	app := application{
 
 		errorLog:      errorLog,
@@ -68,12 +77,16 @@ func main() {
 		templateCache: templateCache,
 		Session:       session,
 	}
-
+	// info text outputted at server start
 	infoLog.Printf("Starting server on %s", *addr)
+
+	// select cypher type for tls (makes message sending secure)
 	tlsConfig := &tls.Config{
 		PreferServerCipherSuites: true,
 		CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP256},
 	}
+
+	// setting for the server
 	srv := &http.Server{
 		Addr:         *addr,
 		ErrorLog:     errorLog,
